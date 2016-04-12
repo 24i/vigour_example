d6693120e6eabc0b09671ccd2ddcd7c6a40b6a84
Nuno Frade <nuno@vigour.io>
2016-04-12 12:36:25 +0200

    Css share context test
    Added test for sharing custom properties between different scopes
    (requires)

--- test/index.js
+++ test/index.js
@@ -1,9 +1,15 @@
 'use strict'
 
+// Css to override the component default
+require('./style.css')
+
 var e = require('vigour-element/e')
 
 var app = global.app = e({
   DOM: document.body,
   components: [ require('../lib') ],
-  thing: { type: 'thing' }
+  thing: { type: 'thing' },
+  message: {
+    text: 'In this context, thing should be red'
+  }
 })
